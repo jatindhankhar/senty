7bf760b65ef88715b7b87b1624811dd151064be2
Jatin Dhankhar <dhankhar.jatin@gmail.com>
2019-06-02 10:39:32 +0530

    Update dependencies

--- server.py
+++ server.py
@@ -1,12 +1,14 @@
 #from nltk.sentiment.vader import SentimentIntensityAnalyzer
-from vaderSentiment.vaderSentiment import sentiment
+#from vaderSentiment.vaderSentiment import sentiment
+from io import open
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from flask import Flask
 from flask import request
 from flask import jsonify
 
 app = Flask(__name__,static_url_path='')
 
-
+analyzer = SentimentIntensityAnalyzer()
 @app.route("/")
 def hello():
     return app.send_static_file('index.html')
@@ -16,7 +18,7 @@ def score():
     phrase =  request.args.get('phrase')
     
     if phrase:
-      return jsonify(sentiment(phrase))
+      return jsonify(analyzer.polarity_scores(phrase))
     else:
       return jsonify({"error" : "Phrase cannot be empty"})
 
